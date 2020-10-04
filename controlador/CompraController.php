8d54b26182260b34765dab8d2fea0242f903c724
albinojovel2020 <albinopaypal@gmail.com>
2020-10-04 00:44:36 -0600

    Creacion de diseño de formulario de compra de productos

---
+++ controlador/CompraController.php
@@ -0,0 +1,255 @@
+<?php
+
+require_once 'modelo/Usuario.php';
+require_once 'modelo/TipoUsuario.php';
+require_once 'modelo/PreguntaSecreta.php';
+
+
+/**
+ *
+ */
+class UsuarioController
+{
+	private $model;
+	private $modelTipoUsuario;
+	private $modelPreguntaSecreta;
+	
+
+	public function __CONSTRUCT()
+	{
+		//Inicializa los modelos
+		$this->model = new Usuario();
+		$this->modelTipoUsuario = new TipoUsuario();
+		$this->modelPreguntaSecreta = new PreguntaSecreta();
+		
+
+	}
+
+	//Tablas de Usuario
+	public function Index(){
+		//muestra todas las partes de la vista de Usuario Almacenado
+		require_once 'vistas/pages/encabezadopagina1.php';
+		require_once 'vistas/pages/verdatos/verusuarios.php';
+		require_once 'vistas/pages/piepagina1.php';
+	}
+
+	//Crear Usuario Nuevo
+	public function CrearUsuario(){
+		//Todas las partes de la vista
+		require_once 'vistas/pages/encabezadopagina1.php';
+		require_once 'vistas/pages/registrar/crearusuario.php';
+		require_once 'vistas/pages/piepagina.php';
+	}
+
+	//Registrar Usuario
+	public function RegistrarUsuario(){
+		//tomar todos los datos
+		date_default_timezone_set("America/Guatemala");
+		$fecha = date('Y-m-d');
+		$horas = date('h:i:s');
+		$tiempo = date('A');
+		$mifecha = $fecha.' a las '.$horas .' '.$tiempo;
+		$this->model->nombre = $_REQUEST['nombres'];
+		$this->model->apellido = $_REQUEST['apellidos'];
+		$this->model->telefono = $_REQUEST['telefono'];
+		$this->model->usuario = $_REQUEST['usuario'];
+		$this->model->clave = md5($_REQUEST['clave1']);
+		$this->model->fecha = $mifecha;
+		$this->model->idpreguntasecreta = $_REQUEST['idpreguntasecreta'];
+		$this->model->respuestasecreta = md5($_REQUEST['respuestasecreta1']);
+		$this->model->idtipousuario = $_REQUEST['idtipousuario'];
+		
+
+		//Guardar Usuario
+		$this->model->RegistrarUsuario($this->model);
+
+		//La vista de usuarios registrados
+		echo "<script>
+		alert('CORRECTO: Los datos fueron guardados.');
+		window.location.href='?c=".base64_encode('Usuario')."';
+		</script>";
+	}
+
+	//Editar Usuario
+	public function EditarUsuario(){
+		//$usuario = new Usuario();
+		//Tomar el id
+		$idusuario = base64_decode($_REQUEST['idusuario']);
+
+		//Obtener el registro con ese id
+		$usuario = $this->model->ObtenerUsuario($idusuario);
+
+		//muestra todas las partes de la vista Registrar
+		require_once 'vistas/pages/encabezadopagina1.php';
+		require_once 'vistas/pages/actualizar/actualizarusuario.php';
+		require_once 'vistas/pages/piepagina.php';
+	}
+
+	public function Ver_datos_usuario(){
+		$idusuario = base64_decode($_REQUEST["idusuario"]);
+
+        //obtener el registro con ese id
+        $usuario = $this->model->ObtenerUsuario($idusuario);
+		require_once 'vistas/pages/encabezadopagina1.php';
+		require_once 'vistas/pages/verdatos/ver_datos_usuario.php';
+		require_once 'vistas/pages/piepagina.php';
+	}
+
+	public function RecuperarPassword(){
+
+
+		require_once 'vistas/pages/encabezadopagina.php';
+		require_once 'vistas/login/recuperar_pass.php';
+		require_once 'vistas/pages/piepagina.php';
+		
+	}
+	public function RespuestasecretaNoCoiciden(){
+		require_once 'vistas/pages/encabezadopagina.php';
+		require_once 'vistas/login/error_restablecimiento.php';
+		require_once 'vistas/pages/piepagina.php';
+	}
+
+	public function Recuperar(){
+		$user= new Usuario();
+		$user = $_REQUEST['usuario'];
+		$preguntasecreta = $_REQUEST['idpreguntasecreta'];
+		$respuestasecreta =md5($_REQUEST['respuestasecreta1']);
+		$user = $this->model->ConsulUser($user, $preguntasecreta, $respuestasecreta);
+
+		$this->model->Cambio($user);
+	}
+
+	public function NuevaPassword(){
+		require_once 'vistas/pages/encabezadopagina.php';
+		require_once 'vistas/login/from_pass.php';
+		require_once 'vistas/pages/piepagina.php';
+	}
+
+	public function ActualizarContraseña(){
+		date_default_timezone_set("America/Guatemala");
+		$fecha = date('Y-m-d');
+		$horas = date('h:i:s');
+		$tiempo = date('A');
+		$mifecha = $fecha.' a las '.$horas .' '.$tiempo;
+		$idusuario=$_REQUEST['idusuario'];
+		$nuevapass=md5($_REQUEST['n_pass1']);
+		$fecha=$mifecha;
+		$this->model->CambioPass($nuevapass, $fecha, $idusuario);
+
+		echo "<script>
+		alert('Su contraseña se modifico con exito.');
+		window.location.href='?c=".base64_encode('Login')."';
+		</script>";
+
+	}
+
+	public function CambiarPasss(){
+		//$usuario = $this->model->ObtenerUsuario($_SESSION["id"]);
+
+        //muestra todas las partes de la vista create
+        require_once 'vistas/pages/encabezadopagina.php';
+        require_once 'vistas/pages/actualizar/cambiarpasss.php';
+        require_once 'vistas/pages/piepagina.php';
+        
+	}
+
+	public function NuevaPasswords(){
+		require_once 'vistas/pages/encabezadopagina.php';
+		require_once 'vistas/pages/actualizar/nueva_pass.php';
+		require_once 'vistas/pages/piepagina.php';
+	}
+	public function Recuperarr(){
+		//$user= new Usuario();
+		$user = $_REQUEST['idusuario'];
+		$actual = md5($_REQUEST['n_passs1']);
+		//$respuestasecreta =md5($_REQUEST['respuestasecreta1']);
+		$user = $this->model->ConsulActual($user, $actual);
+
+		$this->model->Cambioo($user);
+	}
+	public function RespuestasecretaNoCoicidenn(){
+		require_once 'vistas/pages/encabezadopagina.php';
+		require_once 'vistas/pages/actualizar/error_resta.php';
+		require_once 'vistas/pages/piepagina.php';
+	}
+
+	public function ActualizarContraseñaa(){
+		date_default_timezone_set("America/Guatemala");
+		$fecha = date('Y-m-d');
+		$horas = date('h:i:s');
+		$tiempo = date('A');
+		$mifecha = $fecha.' a las '.$horas .' '.$tiempo;
+		$idusuario=$_REQUEST['idusuario'];
+		//$actual = md5($_REQUEST['n_passs1']);
+		$nuevapass=md5($_REQUEST['n_pass1']);
+		$fecha=$mifecha;
+		$this->model->CambioPasss($nuevapass, $fecha, $idusuario);
+
+		echo "<script>
+		alert('Su contraseña se modifico con exito.');
+		window.location.href='?c=".base64_encode('Login')."';
+
+		</script>";
+		//&a=".base64_encode('Ver_datos_usuario')."&idusuario=".base64_encode($_SESSION["id"])."
+	}
+
+	
+
+	//Actualizar usuario
+	public function ActualizarUsuario(){
+		//Tomar todos los datos
+		date_default_timezone_set("America/Guatemala");
+		$fecha = date('Y-m-d');
+		$horas = date('h:i:s');
+		$tiempo = date('A');
+		$mifecha = $fecha.' a las '.$horas .' '.$tiempo;
+		//$usuario_actual = $this->model->ObtenerUsuarios($id_usuario);
+		$this->model->idusuario = $_REQUEST['idusuario'];
+		$this->model->nombre = $_REQUEST['nombres'];
+		$this->model->apellido = $_REQUEST['apellidos'];
+		$this->model->telefono = $_REQUEST['telefono'];
+		$this->model->fecha = $mifecha;
+		$this->model->usuario = $_REQUEST['usuario'];
+		$this->model->idtipousuario = $_REQUEST['idtipousuario'];
+
+		//Actualiza el Usuario
+		$this->model->ActualizarUsuario($this->model);
+
+		//La vista de usuario y muestra la actualizacion de x usuario
+		echo "<script>
+		alert('CORRECTO: Registro Modificado.');
+		window.location.href='?c=".base64_encode('Usuario')."';
+		</script>";
+
+	}
+
+	//CambiarEstado
+	public function CambiarEstado(){
+		//tomar id y nuevo estado
+		$idusuario = base64_decode($_REQUEST['idusuario']);
+		$nuevo_estado = base64_decode($_REQUEST['nuevo_estado']);
+
+		//cambiar estado
+		$this->model->CambiarEstadoUsuario($nuevo_estado, $idusuario);
+
+		//muestra la vista de usuario
+		echo "<script>
+		alert('CORRECTO: Registro Modificado.');
+		window.location.href='?c=".base64_encode('Usuario')."';
+		</script>";
+	}
+
+	public function Eliminar(){
+		//Tomar el id
+		$idusuario = base64_decode($_REQUEST['idusuario']);
+
+		$this->model->EliminarUsuario($idusuario);
+		echo "<script>
+		alert('Registro ELIMINADO.');
+		window.location.href='?c=".base64_encode('Usuario')."';
+		</script>";
+	}
+
+}
+
+?>
---
+++ vistas/pages/registrar/crearcompra.php
@@ -0,0 +1,118 @@
+
+<!-- inicio del cuerpo -->
+<script language="JavaScript">
+   function validaselecttipousu(){
+    if (document.frm.idtipousuario.value == "")
+     alert("Favor asignar un tipo de usuario");
+    else
+     document.frm.idtipousuario.submit();
+   }
+</script>
+
+<!-- inicio del cuerpo -->
+<nav>
+  <div class="nav-wrapper white  ">
+    <ul id="nav-mobile" class="left hide-on-med-and-down">
+      <li ><a class="grey-text text-darken-1"><b>DISTRIBUIDORA ESPECIAL > USUARIOS</b></a></li> 
+    </ul>
+    <ul class="right">
+      <li><a class="waves-effect waves-light btn modal-trigger grey lighten-4 grey-text text-darken-1" href="?c=<?php echo base64_encode('Usuario'); ?>" ><b>Volver</b><i class="material-icons right grey-text text-darken-1">arrow_back</i></a></li>
+    </ul>
+  </div>
+</nav>
+<br>  
+<div class="container">
+
+  <div class="card-panel grey lighten-5 blue-text">
+   <div class="section">
+
+    <!--   Form Section   -->
+    <div class="row form-background">
+      <form name="frm"class="col s12" action="?c=<?php echo base64_encode('Usuario'); ?>&a=<?php echo base64_encode('RegistrarUsuario'); ?>" method="post">
+        <div class="row top20px bottom20px">
+            <div class="input-field col s12"> 
+
+                <select id="idtipousuario" name="idtipousuario" class="validate" required>      <option value="" disabled selected>Seleccione el tipo de usuario</option>            
+                    <?php foreach($this->modelTipoUsuario->ListarTiposUsuarios() as $r): ?>
+                        <option value="<?php echo $r->idtipousuario; ?>"><?php echo $r->nombre; ?></option>
+                    <?php endforeach; ?>
+                  </select>
+            </div>
+
+            <div class="input-field col s12 m6">
+              <i class="material-icons prefix form-icon">person</i>
+              <input  id="nombres" type="text" class="validate" name="nombres" required autofocus>
+              <label for="nombres">Nombres</label>
+            </div>
+
+            <div class="input-field col s12 m6">
+              <i class="material-icons prefix form-icon">person_outline</i>
+              <input id="apellidos" type="text" class="validate" name="apellidos" required>
+              <label for="apellidos">Apellidos</label>
+            </div>
+
+            <div class="input-field col s12 m6">
+              <i class="material-icons prefix form-icon">phone</i>
+              <input id="telefono" type="text" class="validate" name="telefono" required onkeyup="mascara(this,'-',patron3,true);" onkeypress="return justNumbers(event);">
+              <label for="telefono">Teléfono</label>
+            </div>
+
+            <div class="input-field col s12 m6">
+              <i class="material-icons prefix form-icon">account_circle</i>
+              <input id="usuario" type="text" class="validate" name="usuario" required>
+              <label for="usuario">Usuario</label>
+            </div>
+
+            <div class="input-field col s12 m6">
+              <i class="material-icons prefix form-icon">vpn_key</i>
+              <input id="input_text" id="clave1" type="password"  class="validate" name="clave1" minlength="6" maxlength="12" required>
+              <label for="clave1">Clave</label>
+            </div>
+
+            <div class="input-field col s12 m6">
+              <i class="material-icons prefix form-icon">check_circle</i>
+              <input id="input_text" id="clave2" type="password" class="validate" name="clave2" minlength="6" maxlength="12" required>
+              <label for="clave2">Confirmar clave</label>
+            </div>
+            <div class="input-field col s12">              
+            <select id="idpreguntasecreta" name="idpreguntasecreta" class="validate" required>          <option value="" disabled selected>Seleccione una pregunta secreta</option>          
+                <?php foreach($this->modelPreguntaSecreta->ListarPreguntasSecretas() as $r): ?>
+                    <option value="<?php echo $r->idpreguntasecreta; ?>"><?php echo $r->nombre; ?></option>
+                <?php endforeach; ?>
+              </select>
+            </div>
+
+            <div class="input-field col s12 m6">
+              <i class="material-icons prefix form-icon">help</i>
+              <input id="respuestasecreta1" type="password" class="validate" name="respuestasecreta1" minlength="4" maxlength="10" required>
+              <label for="respuestasecreta1">Respuesta</label>
+            </div>
+
+            <div class="input-field col s12 m6">
+              <i class="material-icons prefix form-icon">check_circle</i>
+              <input id="respuestasecreta2" type="password" class="validate" name="respuestasecreta2" minlength="4" maxlength="10" required>
+              <label for="respuestasecreta2">Confirmar respuesta</label>
+            </div> 
+            <div class="input-field col s12">
+              <center>
+                <button class="btn waves-effect waves-light green accent-4 hoverable" type="submit" name="guardar" title="Guardar registro" onclick="validaselecttipousu()">Guardar
+                <i class="material-icons right">save</i>
+              </button>
+              </center>
+              
+            </div>
+
+          </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+</div>
+<div class="container">
+    <!-- floating button -->
+    <div class="fixed-action-btn">
+    <a href="?c=<?php echo base64_encode('Login');?>&a=<?php echo base64_encode('CerrarSesion');?>" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">exit_to_app</i></a>
+</div>
+<!-- fin del cuerpo -->
